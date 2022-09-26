49ed44a2938b16254843396a142e085a31729dc6
robertdpowell <robertdpowell@gmail.com>
2022-09-26 06:52:27 +0100

    test commit

--- hex.py
+++ hex.py
@@ -1,6 +1,6 @@
 def hex_output():
     decnum = 0
-    hexnum = input('Enter a hex number to convert: ')
+    hexnum = input('Please enter a hex number to convert: ')
     for power, digit in enumerate(reversed(hexnum)):
         print(f'{digit}: * 16  ** {power}')
         decnum += int(digit, 16) * (16 ** power)
